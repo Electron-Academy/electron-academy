9327801fe1692ddc7099546df4ee68c8378ad550
Electron-Academy <68534575+Electron-Academy@users.noreply.github.com>
2020-07-20 15:12:59 +0400

    Added the code files for Python #2 (Variables)
    I added the code files for Python #2 (Variables) video.

---
+++ Python #2/Code Files/main.py
@@ -0,0 +1,21 @@
+var = 2
+print(var)
+var = 1
+print(var)
+
+var2 = "string"
+print(var2)
+var2 = "not a string"
+print(var2)
+
+result = 3 + 9
+print(result)
+
+num1 = 5.8
+num2 = 8.2
+
+result2 = num1 + num2
+print(result2)
+
+result3 = 7 + num2
+print(result3)
