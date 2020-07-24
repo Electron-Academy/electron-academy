643ab22e480a3ab189ac9c0ab0c176dd05b9e2ac
Electron-Academy <68534575+Electron-Academy@users.noreply.github.com>
2020-07-24 22:42:41 +0400

    Added Python #3 (Mathematical Operations) Folder
    I added the folder for Python #3 (Mathematical Operations) video.

---
+++ Python #3/Code Files/main.py
@@ -0,0 +1,18 @@
+print(8 + 5)
+
+result = 7 - 8
+print(result)
+
+num = 8 * 5
+print(num)
+
+num2 = 12 / 6
+print(num2)
+
+print(num / num2)
+
+num3 = 7 * 7
+print(num3 + 1)
+
+num4 = 8 * 8
+print(num4 * (num / num2))
