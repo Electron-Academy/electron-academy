3bd217ae2e3bc61a1156172a3762e292ca33c29c
Electron-Academy <68534575+Electron-Academy@users.noreply.github.com>
2020-07-24 22:39:12 +0400

    Added Python #4 (Escape Characters) Folder
    I added the Python #4 (Escape Characters) video folder to the repository.

---
+++ Python #4/Code Files/main.py
@@ -0,0 +1,9 @@
+print("He said: \'I like fishes\'")
+print("He said: \nI like fishes")
+
+print("He said: \n\'I like fishes\'")
+
+print("He said: Alen\\Georgia stole my beans")
+print("\tDear Maam,\n\nJohnny was very mean today. Please deal with him later. \n\nSincerely, \nThe Manager")
+
+print("He said:  \be")
